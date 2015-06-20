f963ac43811bd6bfd8edbbd7f6ba115a79161e2c
Rick Benjamin Lye <appsoluzion@gmail.com>
2015-06-21 05:02:42 +0800

    updated cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -7,13 +7,13 @@
 makeCacheMatrix <- function(mtx = matrix()) {
     inverse <- NULL
     set <- function(x) {
-        mtx <<- x;
-        inverse <<- NULL;
+        mtx <<- x
+        inverse <<- NULL
     }
-    get <- function() return(mtx);
-    setinv <- function(inv) inverse <<- inv;
-    getinv <- function() return(inverse);
-    return(list(set = set, get = get, setinv = setinv, getinv = getinv))
+    get <- function() mtx
+    setinv <- function(inv) inverse <<- inv
+    getinv <- function() inverse
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 ## This function computes the inverse of the special
@@ -30,15 +30,9 @@ cacheSolve <- function(mtx, ...) {
     data <- mtx$get()
     invserse <- solve(data, ...)
     mtx$setinv(inverse)
-    return(inverse)
+    inverse
 }
-        inverse <<- NULL;
-    }
-    get <- function() return(mtx);
-    setinv <- function(inv) inverse <<- inv;
-    getinv <- function() return(inverse);
-    return(list(set = set, get = get, setinv = setinv, getinv = getinv))
-}
+      
 
 
 
