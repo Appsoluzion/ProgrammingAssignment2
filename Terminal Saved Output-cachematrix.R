b0f2668ca8d1d04f51e84a02599c2c8c210a7543
Rick Benjamin Lye <appsoluzion@gmail.com>
2015-06-21 03:02:58 +0800

    updated the cachematrix file

---
+++ Terminal Saved Output-cachematrix.R
@@ -0,0 +1,24 @@
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
+
+cacheSolve <- function(mtx, ...) {
+    inverse <- mtx$getinv()
+    if(!is.null(inverse)) {
+        message("Getting cached data...")
+        return(inverse)
+    }
+    data <- mtx$get()
+    invserse <- solve(data, ...)
+    mtx$setinv(inverse)
+    return(inverse)
+}
+
+
+:wq
+
+
+
+
+
+
